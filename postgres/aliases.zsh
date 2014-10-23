8367058f8a6ce2397c34481059bb90e26b6dd765
Guilherme Ferreira <guilffer@gmail.com>
2014-10-23 13:08:25 -0200

    alias to show postgres startup log file

--- postgres/aliases.zsh
+++ postgres/aliases.zsh
@@ -1,3 +1,4 @@
 alias pg-start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias pg-stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 alias pg-status='pg_ctl -D /usr/local/var/postgres status'
+alias pg-log='tail /usr/local/var/postgres/server.log'
